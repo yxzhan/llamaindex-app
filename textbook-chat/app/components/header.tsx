fb0771d888bd8bb4de00bb9ac3082c3371944285
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-04-16 17:01:02 +0200

    delete header element

--- textbook-chat/app/components/header.tsx
+++ textbook-chat/app/components/header.tsx
@@ -6,22 +6,6 @@ export default function Header() {
       <p className="fixed left-0 top-0 flex w-full justify-cesnter border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
       Talk to the AICOR textbook
       </p>
-      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-        <a
-          href="https://www.llamaindex.ai/"
-          className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
-        >
-          <span>Built by LlamaIndex</span>
-          <Image
-            className="rounded-xl"
-            src="/llama.png"
-            alt="Llama Logo"
-            width={40}
-            height={40}
-            priority
-          />
-        </a>
-      </div>
     </div>
   );
 }

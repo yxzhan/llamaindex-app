9718be08ce04d08cd609d3781e53edcb1151d3d2
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-04-17 17:09:58 +0200

    Used asset prefix

--- textbook-chat/next.config.mjs
+++ textbook-chat/next.config.mjs
@@ -5,10 +5,12 @@ import webpack from "./webpack.config.mjs";
 const nextConfig = JSON.parse(fs.readFileSync("./next.config.json", "utf-8"));
 nextConfig.webpack = webpack;
 
-// set basePath if running on binderhub
+// // set url prefix if running on binderhub
 const jp_prefix = process.env.JUPYTERHUB_SERVICE_PREFIX
 if (jp_prefix !== undefined) {
-    nextConfig.basePath = jp_prefix + "proxy/3000"
+    // nextConfig.basePath = jp_prefix + "proxy/3000"
+    nextConfig.assetPrefix = jp_prefix + "proxy/3000"
+    process.env.NEXT_PUBLIC_CHAT_API = 'https://jupyter.intel4coro.de' + jp_prefix + "proxy/3000/api/chat"
 }
 
 export default nextConfig;

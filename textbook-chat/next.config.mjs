4325f4fb8540b754533b18867c43b2c8d6f53c1b
Yanxiang Zhan <yanxiang@uni-bremen.de>
2024-04-17 15:39:26 +0200

    add binderhub basepath

--- textbook-chat/next.config.mjs
+++ textbook-chat/next.config.mjs
@@ -5,4 +5,10 @@ import webpack from "./webpack.config.mjs";
 const nextConfig = JSON.parse(fs.readFileSync("./next.config.json", "utf-8"));
 nextConfig.webpack = webpack;
 
+// set basePath if running on binderhub
+const jp_prefix = process.env.JUPYTERHUB_SERVICE_PREFIX
+if (jp_prefix !== undefined) {
+    nextConfig.basePath = jp_prefix + "proxy/3000"
+}
+
 export default nextConfig;
